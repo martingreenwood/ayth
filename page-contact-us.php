13eaa395e98218b1f02966880d0b6eef95a108c6
Martin Greenwood <martin@wearewbeard.com>
2018-06-28 09:31:55 +0100

    updates

--- page-contact-us.php
+++ page-contact-us.php
@@ -32,6 +32,12 @@ get_header(); ?>
 			</div>
 			<?php endif ?>
 
+			<ul class="social">
+				<li><a target="_blank" href="https://www.facebook.com/Athelyn-Ltd-1764540426899802/" title=""><i class="fab fa-facebook-f"></i></a></li>
+				<li><a target="_blank" href="https://twitter.com/AthelynLtd" title=""><i class="fab fa-twitter"></i></a></li>
+				<li><a target="_blank" href="https://www.linkedin.com/company/athelyn-ltd/" title=""><i class="fab fa-linkedin-in"></i></a></li>
+			</ul>
+
 		</main>
 	</div>
 
